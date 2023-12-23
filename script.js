97cf8897233de9102812817f31f48deb95df76ca
Megan Mulcahy <megan.mulcahy03@gmail.com>
2023-12-23 16:31:47 -0600

    overwriting history so you dont add a bunch of history when navigating from top bar

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
-console.log(header)
 window.onscroll = function() {
     const top = window.scrollY;
-    if(top >=100) {
+    if (top >= 100) {
         header.classList.add('navbarDark');
-    }
-    else {
+    } else {
         header.classList.remove('navbarDark');
     }
 }
+
 // collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
+const navLinks = document.querySelectorAll('.nav-link');
 const menuToggle = document.getElementById('navbarSupportedContent')
 
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-    history.replaceState(null, null, `#${targetId}`);
-})
+    l.addEventListener('click', (event) => {
+        // Your logic for handling the navigation click goes here
+
+        // Overwrite the history entry
+        history.replaceState(null, null, `#${event.target.getAttribute('href').substring(1)}`);
+        
+        // Toggle the menu
+        new bootstrap.Collapse(menuToggle).toggle();
+    });
+});
